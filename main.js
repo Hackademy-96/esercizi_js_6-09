c4eef15ba5f5e722e0f695414ae07a9c121d9c37
OnofrioSoranno <sorannoonofrio@outlook.it>
2023-09-06 16:28:50 +0200

    fine esercizio 3

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
+// ESERCIZIO NUMERO 1
 let number1= 10;
 let number2=6;
 let number3=12;
 let number4=18;
 let number5= 22;
 let somma= number1+number2+number3+number4+number5;
-console.log(somma);
 let numeroMedia= 5;
-let divisione= somma/numeroMedia;
-console.log(divisione);
+let media= somma/numeroMedia;
+let frase1= "La somma tra gli elementi è "
+let frase2=" e la sua media è "
+console.log(frase1+somma+frase2+media);
+
+
+// ESERCIZIO NUMERO 2
+let annoCorrente= 2023;
+let annoDiNascita= 2001;
+let età= annoCorrente-annoDiNascita;
+console.log(età);
+let cento=100;
+let centenario=cento-età;
+console.log(`Hai ` +età+ ` e ti mancano `+centenario+` anni per compiere `+cento+` anni`)
+
+// ESERCIZIO NUMERO 3 
+let gatti=44;
+let perFila=6;
+let file= gatti/perFila;
+file=7;
+let gattiNelleFile= perFila*file;
+console.log(gattiNelleFile);
+let restoGatti=gatti-gattiNelleFile
+let gattiMancanti= perFila-restoGatti
+console.log(`Ci sono `+file+` file di gatti e ne mancano `+gattiMancanti+` per completare la fila, con il resto di `+restoGatti )
+
